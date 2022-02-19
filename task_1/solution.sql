e2afbabc3651206387d512180343d3b43376779b
Evseev Maxim <65456432+q6q9@users.noreply.github.com>
2022-02-19 04:27:36 +0300

    Update solution.sql

--- task_1/solution.sql
+++ task_1/solution.sql
@@ -1,10 +1,11 @@
 SELECT CONCAT(users.first_name, ' ', users.last_name) as name,
        author,
        GROUP_CONCAT(books.name SEPARATOR ', ')        as books
-
 FROM test.users
          INNER JOIN user_books on users.id = user_books.user_id
          INNER JOIN books on user_books.book_id = books.id
+WHERE users.age > 7
+AND users.age < 17
 GROUP BY users.id
 HAVING COUNT(books.id) = 2
    AND SUBSTRING_INDEX(GROUP_CONCAT(books.author), ',', 1) = SUBSTRING_INDEX(GROUP_CONCAT(books.author), ',', -1)
